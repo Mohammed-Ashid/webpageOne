e3827958e536e74e47971facc74f8b68148644dd
Mohammed-Ashid <mohammedashid773@gmail.com>
2024-11-10 15:07:04 +0530

    implemeted review component

--- src/Components/IndexWeb.jsx
+++ src/Components/IndexWeb.jsx
@@ -5,6 +5,7 @@ import '../Styles/IndexWeb.css'
 import About from './About'
 import Milestone from './Milestones'
 import Gallery from './UpcomingTrips'
+import ReviewComponent from './ReviewComponent'
 function IndexWeb() {
   return (
     <div className='main'>
@@ -14,6 +15,7 @@ function IndexWeb() {
         <h4 className='upcomingHeading'>UPCOMING TRIPS</h4>
         <Gallery/>
         <About/>
+        <ReviewComponent/>
       
     </div>
   )
---
+++ src/Components/ReviewComponent.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../Styles/ReviewComponent.css'
+import reviewImg1 from '/Images/Screenshot_20240202-165344~2.png'
+function ReviewComponent() {
+  return (
+    <div className='ReviewComponent-main-div'>
+        <div className='ReviewBox'>
+            <div className='ReviewImage'>
+                <img src={reviewImg1} alt=""/>
+            </div>
+            <div className='ReviewComment'>
+<h1>Fathima Beevi P s</h1>
+<p>Great experince</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ReviewComponent
